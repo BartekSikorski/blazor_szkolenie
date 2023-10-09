182027c65debc2e4ede615297051a52c544e1772
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-10-09 14:12:53 +0200

    Dodanie rejestracji nazwanego klienta http

--- src/IssueTracker/BlazorClientApp/Program.cs
+++ src/IssueTracker/BlazorClientApp/Program.cs
@@ -1,4 +1,5 @@
 using BlazorClientApp;
+using BlazorClientApp.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7228") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// PM> Install-Package Microsoft.Extensions.Http
+builder.Services.AddHttpClient<IssueApiService>(sp => sp.BaseAddress = new Uri("https://localhost:7228"));
 
 await builder.Build().RunAsync();
---
+++ src/IssueTracker/BlazorClientApp/Services/IssueApiService.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Models;
+using System.Net.Http.Json;
+
+namespace BlazorClientApp.Services;
+
+public class IssueApiService
+{
+    private readonly HttpClient client;
+
+    public IssueApiService(HttpClient client)
+    {
+        this.client = client;
+    }
+
+    public Task<IEnumerable<Issue>> GetAllAsync() => client.GetFromJsonAsync<IEnumerable<Issue>>("api/issues");
+    public Task<Issue> GetByIdAsync(int id) => client.GetFromJsonAsync<Issue>($"api/issues/{id}");
+}
