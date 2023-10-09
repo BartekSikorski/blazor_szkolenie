6db0d2d0cc8711f10a611544c63482fe1ec1efdf
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-10-09 11:35:25 +0200

    Dodanie modelu danych

---
+++ src/IssueTracker/Domain/Models/Issue.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Models;
+
+public class Issue : BaseEntity
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public Status Status { get; set; }
+}
+
+public enum Status
+{
+    Open,
+    InProgress,
+    Closed
+}
