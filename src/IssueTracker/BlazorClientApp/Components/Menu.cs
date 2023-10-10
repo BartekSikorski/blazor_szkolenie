84a0741dc47858552e2441561ad86a717a654f35
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-10-10 12:50:38 +0200

    Generowanie dynamiczne kontrolki

---
+++ src/IssueTracker/BlazorClientApp/Components/Menu.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components.Routing;
+
+namespace BlazorClientApp.Components;
+
+public class Menu : ComponentBase
+{
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        base.BuildRenderTree(builder);
+
+        builder.OpenElement(0,"nav");
+        builder.AddAttribute(1, "class", "menu");
+
+        builder.OpenElement(2, "ul");
+
+        builder.OpenElement(3, "li");
+
+        builder.OpenComponent<NavLink>(4);
+        builder.AddAttribute(5, "href", "/");
+        builder.AddAttribute(6, "ChildContent", (RenderFragment) (builder2 =>
+        {
+            builder2.AddContent(7, "Home");
+        }));
+
+        builder.CloseComponent();
+
+        builder.CloseElement();
+
+        builder.CloseElement();
+
+        builder.CloseElement();
+
+    }
+}
