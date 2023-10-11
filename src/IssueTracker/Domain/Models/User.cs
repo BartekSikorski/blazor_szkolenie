baa4d2ca1190c8939bc0f8762ebf9aedc4eeedb7
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-10-11 11:59:29 +0200

    Walidacja za pomocą atrybutów

--- src/IssueTracker/Domain/Models/User.cs
+++ src/IssueTracker/Domain/Models/User.cs
@@ -1,9 +1,14 @@
-﻿namespace Domain.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Models;
 
 public class User : BaseEntity
 {
+    [Required, StringLength(10, MinimumLength = 3)]
     public string FirstName { get; set; }
+    [Required]
     public string LastName { get; set; }
+    [EmailAddress]
     public string Email { get; set; }
     public string Note { get; set; }
     public bool IsSelected { get; set; }
