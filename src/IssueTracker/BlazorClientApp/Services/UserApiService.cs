d596098a63a41c03c7b0a99ea35af338ce8afcb6
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-10-11 16:43:14 +0200

    EventCallbackFactory

--- src/IssueTracker/BlazorClientApp/Services/UserApiService.cs
+++ src/IssueTracker/BlazorClientApp/Services/UserApiService.cs
@@ -12,5 +12,6 @@ public class UserApiService
     public Task<IEnumerable<User>> GetBySearchCriteriaAsync(UserSearchCriteria criteria) => client.GetFromJsonAsync<IEnumerable<User>>($"/api/users/search?{criteria}");
     public Task AddAsync(User user) => client.PostAsJsonAsync("api/users", user);
     public Task UpdateAsync(User user) => client.PutAsJsonAsync($"api/users/{user.Id}", user);
+    public Task RemoveAsync(int id) => client.DeleteAsync($"api/users/{id}");
 
 }
