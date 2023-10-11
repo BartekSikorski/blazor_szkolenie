fe5eb06b499382a75db02c72899eca5f6bd01ae1
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-10-11 15:36:07 +0200

    Okno dialogowe

--- src/IssueTracker/BlazorClientApp/Services/UserApiService.cs
+++ src/IssueTracker/BlazorClientApp/Services/UserApiService.cs
@@ -8,7 +8,9 @@ public class UserApiService
     private readonly HttpClient client;
     public UserApiService(HttpClient client) => this.client = client;
     public Task<IEnumerable<User>> GetAllAsync() => client.GetFromJsonAsync<IEnumerable<User>>("api/users");
+    public Task<User> GetByIdAsync(int id) => client.GetFromJsonAsync<User>($"api/users/{id}");
     public Task<IEnumerable<User>> GetBySearchCriteriaAsync(UserSearchCriteria criteria) => client.GetFromJsonAsync<IEnumerable<User>>($"/api/users/search?{criteria}");
     public Task AddAsync(User user) => client.PostAsJsonAsync("api/users", user);
+    public Task UpdateAsync(User user) => client.PutAsJsonAsync($"api/users/{user.Id}", user);
 
 }
